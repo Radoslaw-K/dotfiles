c7b009d0912f91a8cb78eed85839541029443be2
Radoslaw <rkieltyka@dataplicity.com>
2017-01-14 18:26:53 +0000

    Fixes issue #2

--- installwizard.bash
+++ installwizard.bash
@@ -26,7 +26,7 @@ sudo apt-get install -y vim
 
 echo "[$FUNCNAME] Configuring vim..."
 cp $PARENT_PATH/dotfiles/.vimrc /home/$USER/
-printf "\n\n" >> /home/$USER/.bashrc
+printf "\n" >> /home/$USER/.bashrc
 cat $PARENT_PATH/includes/bashrc_vimrc.update >> /home/$USER/.bashrc
 
 echo "[$FUNCNAME] Setting up vim plugins..."
@@ -41,6 +41,11 @@ sed -i '17s/" //' /home/$USER/.vimrc
 install_git()
 {
 vimcheck=$(vim --version | head -n 1 | tr " " "\n" | head -n 1)
+curlcheck=$(curl --version | head -n 1 | tr " " "\n" | head -n 1)
+
+if [ -z $curlcheck ]; then 
+    sudo apt-get install -y curl
+fi
 
 sudo apt-get install -y git
 
@@ -53,6 +58,12 @@ git remote add wf-specialproj-pub https://github.com/wildfoundry/specialprojects
 git remote add wf-specialproj-priv https://github.com/wildfoundry/specialprojects
 git remote add wf-wfos https://github.com/wildfoundry/wf-os
 git remote add rados https://github.com/Radoslaw-K/rados
+
+curl -o ~/.git-prompt.sh \
+        https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+
+printf "\n" >> /home/$USER/.bashrc
+printf "source ~/.git-prompt.sh\n" >> /home/$USER/.bashrc
 
 if [ $vimcheck == "VIM" ]; then 
     git config --global core.editor vim
@@ -96,6 +107,10 @@ sudo apt-get -y install tree httpie terminator silversearcher-ag strace screen i
 
 install_prompt_strings()
 {
+USER_GIT_PS_LOCATION=8
+USER_PS_LOCATION=11
+ROOT_PS_LOCATION=14
+
 teecheck=$(tee --version | head -n 1 | tr " " "\n" | head -n 1)
 gitcheck=$(git --version | head -n 1 | tr " " "\n" | head -n 1)
 
@@ -103,27 +118,26 @@ if [ -z $teecheck ]; then
     sudo apt-get install -y tee
 fi
 
-if [ $gitcheck == "git" ]; then
-    user_ps_command="export PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w $(__git_ps1 " <%s>") \$\[\033[00m\] '"
-else
+echo "[$FUNCNAME] Installing prompt string for user: $USER..."
+printf "\n" >> /home/$USER/.bashrc
+
+if [ -z $gitcheck ]; then
     echo "[$FUNCNAME] Git not found on the system, will not be part of the string..."
-    user_ps_command="export PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w \$\[\033[00m\] '"
+    cat $PARENT_PATH/includes/bashrc_gitps.update | head -n $USER_PS_LOCATION | tail -n 1 >> /home/$USER/.bashrc
+else
+    cat $PARENT_PATH/includes/bashrc_gitps.update | head -n $USER_GIT_PS_LOCATION | tail -n 1 >> /home/$USER/.bashrc
 fi
 
-root_ps_command="export PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[1;31m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w #\[\033[00m\] '"
-
-echo "[$FUNCNAME] Installing prompt string for user: $USER..."
-echo $user_ps_command >> /home/$USER/.bashrc
-
 echo "[$FUNCNAME] Installing prompt string for user: root..."
-echo $root_ps_command | sudo tee --append /root/.bashrc > /dev/null
+printf "\n" >> /home/$USER/.bashrc
+cat $PARENT_PATH/includes/bashrc_gitps.update | head -n $ROOT_PS_LOCATION | tail -n 1 | sudo tee --append /root/.bashrc > /dev/null
 }
 
 
 install_bashrc_extras()
 {
 echo "[$FUNCNAME] Installing custom bash commands..."
-printf "\n\n" >> /home/$USER/.bashrc
+printf "\n" >> /home/$USER/.bashrc
 cat $PARENT_PATH/includes/bashrc_commands.extra >> /home/$USER/.bashrc
 }
 
