f49e893aedfec370ea67ede0b7c3115284c66e2f
Radoslaw Kieltyka <Radoslaw-K@users.noreply.github.com>
2017-01-14 09:05:42 -0800

    Fixes for issue #1

--- installwizard.bash
+++ installwizard.bash
@@ -27,6 +27,9 @@ cat $PARENT_PATH/includes/bashrc_vimrc.update >> /home/$USER/.bashrc
 echo "[$FUNCNAME] Setting up vim plugins..."
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qall
+
+sed -i '16s/" //' $PARENT_PATH/dotfiles/.vimrc
+sed -i '17s/" //' $PARENT_PATH/dotfiles/.vimrc
 }
 
 
